4ac4b9ac1107df31f0152dfd327414997f8ccd21
Raymond Chandler III <raymond@Raymonds-MacBook-Pro.local>
2015-08-30 13:23:44 -0400

    Sets up rspec for the hero kata pairing with Shane

---
+++ spec/heros_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require 'heros'
+
+RSpec.describe Weapon, "#init" do
+  it "should equal 2 + 2" do
+    expect(2).to eq(2)
+  end
+end
+
+RSpec.describe Hero, "#init" do
+  it "should equal 2 + 2" do
+    expect(2).to eq(2)
+  end
+end
